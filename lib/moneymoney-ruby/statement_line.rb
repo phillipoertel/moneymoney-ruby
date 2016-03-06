2ec3455d6fffe2735e0f7ce2113d3a49f941a572
Phillip Oertel <me@phillipoertel.com>
2016-03-06 18:17:43 +0100

    Even shorter method name

--- lib/moneymoney-ruby/statement_line.rb
+++ lib/moneymoney-ruby/statement_line.rb
@@ -27,38 +27,38 @@ module MoneyMoney
     end
     
     def amount
-      to_cents(value_for(:amount))
+      to_cents(value(:amount))
     end
     
     def booked_on
-      Date.strptime(value_for(:booked_on), '%d.%m.%y')
+      Date.strptime(value(:booked_on), '%d.%m.%y')
     end
     
     def valuta_on
-      Date.strptime(value_for(:valuta_on), '%d.%m.%y')
+      Date.strptime(value(:valuta_on), '%d.%m.%y')
     end
     
     def description
-      value_for(:description).to_s.gsub(/\s+/, ' ')
+      value(:description).to_s.gsub(/\s+/, ' ')
     end
     
     def payee
-      desc_lines = value_for(:description).split("  ").reject { |row| row =~ /\A\s*\Z/ }.map { |row| row.strip }
+      desc_lines = value(:description).split("  ").reject { |row| row =~ /\A\s*\Z/ }.map { |row| row.strip }
       desc_lines[1] || desc_lines[0] || nil
     end
     
     def category
-      [nil, ''].include?(value_for(:category)) ? nil : value_for(:category)
+      [nil, ''].include?(value(:category)) ? nil : value(:category)
     end
     
     def prebooking?
-      !!(value_for(:recipient) =~ /^Ungebuchter Umsatz/)
+      !!(value(:recipient) =~ /^Ungebuchter Umsatz/)
     end
     
     # pass through all attributes which need no modification (currency etc.)
     def method_missing(arg)
       super unless ATTRIBUTE_MAPPING.keys.include?(arg)
-      value_for(arg)
+      value(arg)
     end
     
     def unique_id
@@ -72,7 +72,7 @@ module MoneyMoney
 
     private
 
-    def value_for(key)
+    def value(key)
       csv_key_name = ATTRIBUTE_MAPPING[key]
       @csv_row.fetch(csv_key_name)
     end
