f31d44eac35d9cfa8773d06d8368b788bf1ebef8
Phillip Oertel <me@phillipoertel.com>
2016-03-06 18:01:32 +0100

    Hide access to @row and CSV labels in one method

--- lib/moneymoney-ruby/statement_line.rb
+++ lib/moneymoney-ruby/statement_line.rb
@@ -23,41 +23,42 @@ module MoneyMoney
     }
 
     def initialize(csv_row)
-      @row = csv_row
+      @csv_row = csv_row
     end
     
     def amount
-      to_cents(@row['Betrag'])
+      to_cents(value_for(:amount))
     end
     
     def booked_on
-      Date.strptime(@row['Datum'], '%d.%m.%y')
+      Date.strptime(value_for(:booked_on), '%d.%m.%y')
     end
     
     def valuta_on
-      Date.strptime(@row['Wertstellung'], '%d.%m.%y')
+      Date.strptime(value_for(:valuta_on), '%d.%m.%y')
     end
     
     def description
-      @row['Verwendungszweck'].to_s.gsub(/\s+/, ' ')
+      value_for(:description).to_s.gsub(/\s+/, ' ')
     end
     
     def payee
-      desc_lines = @row['Verwendungszweck'].split("  ").reject { |row| row =~ /\A\s*\Z/ }.map { |row| row.strip }
+      desc_lines = value_for(:description).split("  ").reject { |row| row =~ /\A\s*\Z/ }.map { |row| row.strip }
       desc_lines[1] || desc_lines[0] || nil
     end
     
     def category
-      [nil, ''].include?(@row['Kategorie']) ? nil : @row['Kategorie']
+      [nil, ''].include?(value_for(:category)) ? nil : value_for(:category)
     end
     
     def prebooking?
-      !!(@row['Name'] =~ /^Ungebuchter Umsatz/)
+      !!(value_for(:recipient) =~ /^Ungebuchter Umsatz/)
     end
     
+    # pass through all attributes which need no modification (currency etc.)
     def method_missing(arg)
       super unless ATTRIBUTE_MAPPING.keys.include?(arg)
-      @row.send(:[], ATTRIBUTE_MAPPING[arg])
+      value_for(arg)
     end
     
     def unique_id
@@ -67,6 +68,13 @@ module MoneyMoney
     
     def to_s
       "Buchung vom: #{booked_on}, Betrag: #{amount}, Beschreibung: #{description}"
+    end
+
+    private
+
+    def value_for(key)
+      csv_key_name = ATTRIBUTE_MAPPING[key]
+      @csv_row.fetch(csv_key_name)
     end
     
   end
